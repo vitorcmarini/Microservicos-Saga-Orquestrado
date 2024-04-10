d8b43bead8b4debd601448e377cf0f49c7d867f9
vitor marini <vitorcmarini@hotmail.com>
2024-04-10 11:49:53 -0300

    inventory class.

--- inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/consumer/InventoryConsumer.java
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/consumer/InventoryConsumer.java
@@ -1,6 +1,7 @@
 package br.com.microservices.orchestrated.inventoryservice.core.consumer;
 
 
+import br.com.microservices.orchestrated.inventoryservice.core.service.InventoryService;
 import br.com.microservices.orchestrated.inventoryservice.core.utils.JsonUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class InventoryConsumer {
 
+    private final InventoryService inventoryService;
     private final JsonUtil jsonUtil;
 
     @KafkaListener(
@@ -21,7 +23,7 @@ public class InventoryConsumer {
     public void consumeSuccessEvent(String payload) {
         log.info("Receiving success event {} from inventory-success topic", payload);
         var event = jsonUtil.toEvent(payload);
-        log.info(event.toString());
+        inventoryService.updateInventory(event);
     }
 
     @KafkaListener(
@@ -31,6 +33,6 @@ public class InventoryConsumer {
     public void consumeFailEvent(String payload) {
         log.info("Receiving rollback event {} from inventory-fail topic", payload);
         var event = jsonUtil.toEvent(payload);
-        log.info(event.toString());
+        inventoryService.rollbackInventory(event);
     }
 }
